c15ad4eabeed0d91ae8929063bb5bb0ba7209019
Aniket Gaurav <aniketgaurav001@gmail.com>
2024-09-07 02:06:18 +0530

    updated css

--- main/static/main/scripts.js
+++ main/static/main/scripts.js
@@ -16,3 +16,17 @@ $(document).ready(function() {
 
     showSlide(currentSlide);
 });
+
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(event) {
+        event.preventDefault();
+        
+        var target = $(this.getAttribute('href'));
+        
+        if (target.length) {
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
