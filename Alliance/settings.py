1ac4b8aa16e4c80b3d0f1eec6e9c294039792c64
Aniket Gaurav <aniketgaurav001@gmail.com>
2024-09-07 15:53:57 +0530

    updated about section

--- Alliance/settings.py
+++ Alliance/settings.py
@@ -116,7 +116,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+import os
+from pathlib import Path
+
+
 STATIC_URL = 'static/'
+STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
+
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
